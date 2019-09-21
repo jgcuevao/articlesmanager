3f93576f99a4d5e6d116ae59ef9053d02ef9a7c7
Jenny Cueva <jgcuevao@gmail.com>
2019-09-21 15:42:06 -0500

    Proyecto Inicial

---
+++ articlesmanager/src/main/java/com/mitocode/ArticlesmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mitocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesmanagerApplication.class, args);
+	}
+
+}
